42024c3ff617b164336935928874231da5fecf2d
mont-blanc <86041660+montblanc-github@users.noreply.github.com>
2022-03-13 11:39:30 +0900

    [Add] 解説文追加

--- A_level/cookie_exchanges.rb
+++ A_level/cookie_exchanges.rb
@@ -1,11 +1,14 @@
 a,b,c = gets.split.map(&:to_i)
- 
+
 arr = [a,b,c]
 count = 0
- 
+
+# .uniq.one?ですべて同じ数字かどうか判定
+# .all?(&:even?)ですべて偶数かどうか判定
 if arr.uniq.one? && arr.all?(&:even?)
 	puts -1
 else
+# 配列定義部分が外にあるため、直接指定
   while [a,b,c].all?(&:even?)
   	a_b = a+b
     b_c = b+c
@@ -13,7 +16,7 @@ else
     a = b_c/2
     b = c_a/2
     c = a_b/2
-    count += 1   
+    count += 1
   end
   puts count
 end
