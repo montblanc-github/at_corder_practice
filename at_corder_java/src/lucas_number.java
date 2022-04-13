922b8f8878315bb2b237b69bd60627d23a59b8d0
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-13 13:07:35 +0900

    [Add] B.lucas_number

---
+++ at_corder_java/src/lucas_number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// 提出は、Mainクラスでしないと、コンパイルエラーになる。
+public class lucas_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        // (要注意)配列の要素数は、n+1になる
+        long[] l = new long[n+1];
+        l[0] = 2;
+        l[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            l[i] = l[i-2] + l[i-1];
+        }
+
+        // System.out.println(Arrays.toString(l));
+
+        System.out.println(l[n]);
+
+        sc.close();
+    }
+}
