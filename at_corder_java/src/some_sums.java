5eb50bba7b8797301978c952d16f6449c1ca2d5e
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-16 17:49:27 +0900

    [Add] B.some_sums

---
+++ at_corder_java/src/some_sums.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class some_sums{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next()), a = Integer.parseInt(sc.next()), b = Integer.parseInt(sc.next()), ans = 0;
+        sc.close();
+
+        for(int i = 1; i <= n; i++){
+            int sum = 0, d = i;
+            for(int j = 0; j < 5; j++){
+                sum += d % 10;
+                d /= 10;
+            }
+            if( a <= sum && sum <= b)ans += i;
+        }
+
+        System.out.println(ans);
+    }
+}
