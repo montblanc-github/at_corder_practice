0fc9351d568e710713abb203a4da16dd2d7faf3f
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-17 21:44:37 +0900

    [Add] B.counting_roads

---
+++ at_corder_java/src/counting_roads.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class counting_roads {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next()), m = Integer.parseInt(sc.next());
+
+        // カウンター用配列を用意する。
+        int[] arr = new int[n];
+        Arrays.fill(arr, 0);
+
+        // 入力された都市の個数をカウンター用配列で数える。
+        for(int i=1; i<=m*2; i++){
+            int x = Integer.parseInt(sc.next());
+            arr[x-1] += 1;
+        }
+
+        sc.close();
+
+        for(int val : arr){
+		    System.out.println(val);
+		}
+    }
+}
