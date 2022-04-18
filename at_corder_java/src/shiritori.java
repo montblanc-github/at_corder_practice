9b814333936a4ba21e9e2dc586f03ba35526d64b
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-18 20:39:23 +0900

    [Add] B.shiritori

---
+++ at_corder_java/src/shiritori.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+public class shiritori{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        boolean ans = true;
+        String s = sc.next();
+
+        List<String> list = new ArrayList<String>();
+
+        for(int i=0; i<n-1; i++){
+            String m = sc.next();
+            if((s.charAt(s.length()-1) != m.charAt(0))){
+                ans = false;
+                break;
+            }
+
+            list.add(s);
+            s = m;
+
+            if(list.contains(m)){
+                ans = false;
+                break;
+            }
+        }
+
+        System.out.println(ans ? "Yes" : "No");
+
+    }
+}
