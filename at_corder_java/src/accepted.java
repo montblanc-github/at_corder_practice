5c48e290afedb9033005842b1065b89993a63acd
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-22 16:16:20 +0900

    [Fix] WAになった時点で処理が中断されるよう修正--実行時間のみ早くなる

--- at_corder_java/src/accepted.java
+++ at_corder_java/src/accepted.java
@@ -6,25 +6,29 @@ public class accepted {
         String s = sc.next();
         sc.close();
 
-        boolean ans = true;
         char[] c = s.toCharArray();
         int len = s.length(), cnt = 0;
 
-        if(s.charAt(0) != 'A')ans = false;
+        if(s.charAt(0) != 'A'){
+            System.out.println("WA");
+            System.exit(0);
+        }
 
         for(int i=1; i<len; i++){
             if(Character.isUpperCase(c[i])){
                 if(i==1 || i == len-1 || c[i] != 'C'){
-                    ans = false;
+                    System.out.println("WA");
+                    System.exit(0);
                 }
                 cnt++;
             }
         }
 
         if(cnt != 1){
-            ans = false;
+            System.out.println("WA");
+            System.exit(0);
         }
 
-        System.out.println(ans ? "AC" : "WA");
+        System.out.println("AC");
     }
 }
