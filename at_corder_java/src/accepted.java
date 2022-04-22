aff5d2a8c6adbe26a0f2e83f8da4153d3c7b05b8
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-22 16:20:18 +0900

    [Add] コメント追加

--- at_corder_java/src/accepted.java
+++ at_corder_java/src/accepted.java
@@ -14,6 +14,7 @@ public class accepted {
             System.exit(0);
         }
 
+        // 2文字目以降の小文字判定と、3文字目から末尾から2文字目までの大文字Cの個数カウントを同時に行う処理。
         for(int i=1; i<len; i++){
             if(Character.isUpperCase(c[i])){
                 if(i==1 || i == len-1 || c[i] != 'C'){
