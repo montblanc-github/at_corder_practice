09b07c93e407438aa39b29d3361370d94da2a123
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-24 14:27:31 +0900

    [Add] B.string_rotation

---
+++ at_corder_java/src/string_rotation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class string_rotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next(), t = sc.next();
+        sc.close();
+        int len = s.length();
+        boolean ans = false;
+
+        for(int i = 0; i < len; i++){
+            if(s.equals(t)){
+                ans = true;
+                break;
+            }
+            s = s.substring(len-1) + s.substring(0,len-1);
+        }
+
+        System.out.println(ans ? "Yes" : "No");
+
+    }
+}
