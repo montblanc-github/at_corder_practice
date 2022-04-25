e8f8423d89485948ba9034277ac2c6a799b6970a
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-04-25 09:02:31 +0900

    [Add] B.thin

---
+++ at_corder_java/src/thin.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class thin {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = Integer.parseInt(sc.next()), w = Integer.parseInt(sc.next());
+
+        for(int i=0; i<h; i++){
+            String str = sc.next();
+            for(int j=0; j<2; j++){
+                System.out.println(str);
+            }
+        }
+
+        sc.close();
+    }
+}
