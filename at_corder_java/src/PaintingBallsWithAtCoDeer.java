fd84185be61f119397e57c1243fe655ad16c1e4e
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-05-25 10:19:21 +0900

    [Add] B.PaintingBallsWithAtCoDeer

---
+++ at_corder_java/src/PaintingBallsWithAtCoDeer.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PaintingBallsWithAtCoDeer {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next()),
+            k = Integer.parseInt(sc.next());
+        sc.close();
+
+        long ans = k;
+
+        for(int i=0; i<n-1; i++){
+            ans *= k-1;
+        }
+
+        System.out.println(ans);
+    }
+}
