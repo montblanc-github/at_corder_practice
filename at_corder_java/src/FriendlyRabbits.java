0b9c48483792e22727adfb6e369cf1ffbdea3a8a
mont-blanc087 <nnoa087@gmail.com>
2022-05-26 12:34:42 +0900

    [Add] B.FriendlyRabbits

---
+++ at_corder_java/src/FriendlyRabbits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FriendlyRabbits {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        int[] a = new int[n];
+
+        for(int i=0; i<n; i++){
+            a[i] = Integer.parseInt(sc.next())-1;
+        }
+
+        sc.close();
+
+        int cnt = 0;
+
+        for(int j=0; j<n; j++){
+            if(a[a[j]]==j) cnt++;
+        }
+
+        System.out.println(cnt/2);
+    }
+}
