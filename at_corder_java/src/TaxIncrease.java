cd79daa50571dd68d041ed3f8f6426b4922b140d
mont-blanc087 <nnoa087@gmail.com>
2022-06-22 18:18:40 +0900

    [Add] C.TaxIncrease

---
+++ at_corder_java/src/TaxIncrease.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TaxIncrease {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int A = Integer.parseInt(sc.next()),
+                  B = Integer.parseInt(sc.next());
+
+        for (int i = 0; i < 1001; i++) {
+            int a = i * 8 / 100;
+            int b = i * 10 / 100;
+
+            if (a == A && b == B) {
+                System.out.print(i);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
