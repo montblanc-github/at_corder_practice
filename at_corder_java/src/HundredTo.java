c7e67f45c9c85f90da91b630c3172eeec5b3bf14
mont-blanc087 <nnoa087@gmail.com>
2022-07-02 20:30:26 +0900

    [Add] C.100To105

---
+++ at_corder_java/src/HundredTo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HundredTo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = Integer.parseInt(sc.next());
+        sc.close();
+        int cnt = X / 100;
+        X %= 100;
+
+        System.out.println(X <= cnt*5 ? "1" : "0");
+    }
+}
