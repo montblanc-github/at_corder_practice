60599e87532fa2d04c958bcd4ca6c74009618eae
mont-blanc087 <nnoa087@gmail.com>
2023-04-02 17:22:29 +0900

    [fix] A.CookieExchanges

--- at_corder_java/src/CookieExchanges.java
+++ at_corder_java/src/CookieExchanges.java
@@ -3,31 +3,44 @@ import java.util.Scanner;
 public class CookieExchanges {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = Integer.parseInt(sc.next()),
-            b = Integer.parseInt(sc.next()),
-            c = Integer.parseInt(sc.next());
+        long a = Long.parseLong(sc.next()),
+            b = Long.parseLong(sc.next()),
+            c = Long.parseLong(sc.next());
         sc.close();
+
+        if (isEnd(a, b, c)) {
+            System.out.println(0);
+            System.exit(0);
+        }
 
         if (a == b && b == c) {
             System.out.println(-1);
             System.exit(0);
         }
 
-        int ans = 0;
-        int x,y,z;
+        long ans = 0L;
+        long x,y,z;
 
-        while (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
-            x = a;
-            y = b;
-            z = c;
-            a = (y + z) / 2;
-            b = (x + z) / 2;
-            c = (x + y) / 2;
+        do {
+            x = (b + c) / 2;
+            y = (a + c) / 2;
+            z = (a + c) / 2;
             ans++;
-        }
+            a = x;
+            b = y;
+            c = z;
+        } while (!isEnd(a, b, c));
 
         System.out.println(ans);
     }
+
+    static boolean isEnd(long a, long b, long c) {
+		if ( a%2 == 1 ) { return true; }
+		if ( b%2 == 1 ) { return true; }
+		if ( c%2 == 1 ) { return true; }
+		return false;
+	}
 }
 
-// 問いの通り、コーディング→他2人の合計数の半分を代入→一つでも奇数が出た場合、繰り返し解除→＜結果＞正解。
+// 問いの通り、コーディング→他2人の合計数の半分を代入→一つでも奇数が出た場合、繰り返し解除→＜結果＞正解。一問不正解。ロングにする必要。奇数が含まれている場合の分岐を追加。
+// 苦戦した点：ロング型の場合の初期化にLが必要な点。共通化。代入箇所。
