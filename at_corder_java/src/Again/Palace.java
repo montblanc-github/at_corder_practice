01a2cda8180cc28c93a47c9252f8bdff761fe521
mont-blanc087 <nnoa087@gmail.com>
2023-05-30 21:53:29 +0900

    [add] B.Palace

---
+++ at_corder_java/src/Again/Palace.java
@@ -0,0 +1,29 @@
+package Again;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Palace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next()),
+            t = Integer.parseInt(sc.next()),
+            a = Integer.parseInt(sc.next());
+        Map<Double, Integer> ans = new TreeMap<Double, Integer>();
+ 
+        for(int i=1; i<=n; i++){
+            int x = Integer.parseInt(sc.next());
+            double r = t-x*0.006;
+            double d = Math.abs(r-a);
+            ans.put(d, i);
+        }
+ 
+        sc.close();
+        List<Integer> list = new ArrayList<>(ans.values());
+ 
+        System.out.println(list.get(0));
+    }
+}
