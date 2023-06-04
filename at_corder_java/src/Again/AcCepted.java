47a2d623c67c648bb867c13603991d3a5b1d63e8
mont-blanc087 <nnoa087@gmail.com>
2023-06-04 21:47:06 +0900

    [add] B.AcCepted

---
+++ at_corder_java/src/Again/AcCepted.java
@@ -0,0 +1,36 @@
+package Again;
+
+import java.util.Scanner;
+
+public class AcCepted {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        sc.close();
+
+        char[] c = s.toCharArray();
+        int len = s.length(), cnt = 0;
+ 
+        if(s.charAt(0) != 'A'){
+            System.out.println("WA");
+            System.exit(0);
+        }
+ 
+        for(int i=1; i<len; i++){
+            if(Character.isUpperCase(c[i])){
+                if(i==1 || i == len-1 || c[i] != 'C'){
+                    System.out.println("WA");
+                    System.exit(0);
+                }
+                cnt++;
+            }
+        }
+ 
+        if(cnt != 1){
+            System.out.println("WA");
+            System.exit(0);
+        }
+ 
+        System.out.println("AC");
+    }
+}
