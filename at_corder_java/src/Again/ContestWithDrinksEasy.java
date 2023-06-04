8c0fdfa20ccd99aa21c4670c8a8cd68c3c5d217a
mont-blanc087 <nnoa087@gmail.com>
2023-06-04 21:52:39 +0900

    [add] B.ContestWithDrinksEasy

---
+++ at_corder_java/src/Again/ContestWithDrinksEasy.java
@@ -0,0 +1,31 @@
+package Again;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ContestWithDrinksEasy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        int[] q = new int[n], ans = new int[n];
+
+        for(int i=0; i<n; i++){
+            q[i] = Integer.parseInt(sc.next());
+        }
+
+        int m =Integer.parseInt(sc.next());
+
+        for(int j=0; j<m; j++){
+            int k = Integer.parseInt(sc.next()), sum = 0;
+            ans = Arrays.copyOf(q, n);
+            ans[k-1] = Integer.parseInt(sc.next());
+            for (int val : ans){
+                sum += val;
+            }
+            System.out.println(sum);
+        }
+
+        sc.close();
+    }
+}
+
