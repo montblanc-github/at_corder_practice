d57a95506f1201400c91eae200a494c2f24cdde4
mont-blanc087 <nnoa087@gmail.com>
2023-06-18 21:28:21 +0900

    [add] B.TrainingCamp

---
+++ at_corder_java/src/Again/TrainingCamp.java
@@ -0,0 +1,18 @@
+package Again;
+
+import java.util.Scanner;
+
+public class TrainingCamp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        long ans=1, m =(long) Math.pow(10, 9)+7;
+        sc.close();
+ 
+        for(int i=1; i<=n; i++){
+            ans = ans*i%m;
+        }
+ 
+        System.out.println(ans);
+    }
+}
