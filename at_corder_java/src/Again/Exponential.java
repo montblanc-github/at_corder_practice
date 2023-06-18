9b8ee8a7fb0cc6b04238a85f95e9e1215f23ecfe
mont-blanc087 <nnoa087@gmail.com>
2023-06-18 20:56:34 +0900

    [add] B.Exponential

---
+++ at_corder_java/src/Again/Exponential.java
@@ -0,0 +1,27 @@
+package Again;
+
+import java.util.Scanner;
+
+public class Exponential {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = Integer.parseInt(sc.next()),
+            ans = 0;
+        sc.close();
+ 
+        if(x==1){
+            System.out.println(1);
+            System.exit(0);
+        }
+ 
+        for(int i=2; i<=x; i++){
+            int num = i*i;
+            while(num<=x){
+                ans = Math.max(ans, num);
+                num *= i;
+            }
+        }
+ 
+        System.out.println(ans);
+    }
+}
