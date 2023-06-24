582d5067879b1bc5f04390d58b8998ff20b2065d
mont-blanc087 <nnoa087@gmail.com>
2023-06-25 02:53:54 +0900

    [add] B.IronBarCutting

---
+++ at_corder_java/src/Again/IronBarCutting.java
@@ -0,0 +1,29 @@
+package Again;
+
+import java.util.Scanner;
+
+public class IronBarCutting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        long[] a = new long[n];
+        long s = 0;
+ 
+        for(int i=0; i<n; i++){
+            a[i] = Long.parseLong(sc.next());
+            s += a[i];
+        }
+ 
+        sc.close();
+ 
+        long min = s,
+             sum = 0;
+ 
+        for(int j=0; j<n; j++){
+            sum += a[j];
+            min = Math.min(min, Math.abs(sum-(s-sum)));
+        }
+ 
+        System.out.println(min);
+    }
+}
