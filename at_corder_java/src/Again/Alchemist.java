e639fa017d2163b0f85c36ebfae0476a175d9570
mont-blanc087 <nnoa087@gmail.com>
2023-07-02 05:19:56 +0900

    [add] C.Alchemist

---
+++ at_corder_java/src/Again/Alchemist.java
@@ -0,0 +1,28 @@
+package Again;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Alchemist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                int n = Integer.parseInt(sc.next());
+        int[] v = new int[n];
+ 
+        for(int i=0; i<n; i++){
+            v[i] = Integer.parseInt(sc.next());
+        }
+ 
+        sc.close();
+ 
+        Arrays.sort(v);
+        double a = v[0];
+ 
+        for(int k=0; k<n; k++){
+            a += v[k];
+            a = a/2.0;
+        }
+ 
+        System.out.println(a);
+    }
+}
